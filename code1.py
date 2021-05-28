55dabe559fd9d2481fe32c10289ed876e5b43737
joycethomas-123 <78149448+joycethomas-123@users.noreply.github.com>
2021-05-28 14:26:40 +0530

    Update code1.py

--- code1.py
+++ code1.py
@@ -1,8 +1,83 @@
-#my first code in python
-# Display product of any two numbers by taking value from the user.
+#my first codes in python
+#1: Display product of any two numbers by taking value from the user.
 
 """
 x = input("enter the first number: ")
 y = input("enter the second number: ")
 print(int(x) * int(y))
 """
+
+
+#2: Display the reminder when any two numbers divide each other.(Both x/y and y/x)
+
+"""
+a = input("Enter the first number: ")
+b =  input("Enter the second number: ")
+print(a%b)
+print(b%a)
+"""
+
+
+"""
+3: Display the message “My name is your_name. I am X years old”.
+(Type in your name in the place of “your_name” and store your age inside a
+variable and use that variable to display your age in the place of “X” in the
+message.)
+"""
+"""
+x = input("enter the name: ")
+y = input("enter the age: ")
+
+print('My name is',x,'. I am',y,'years old')
+"""
+
+
+"""
+4: Find the division of two numbers and display the message:
+“Value after division is: division_value”. Take all inputs from user and also display
+the data type of all the variables used. (“division_value” should be replaced with
+the actual value obtained after division.)
+"""
+"""
+h = int(input("enter the first value: "))
+j = int(input("enter the second value: "))
+z = h/j
+print('value after division is:',z)
+print(type(h))
+print(type(j))
+print(type(z))
+"""
+
+
+"""
+5: Perform the same as above given question (Q 4) but, instead of Dividing the
+two numbers and finding the actual value, Divide the two numbers and only
+display the integer part of the value obtained.
+"""
+"""
+h = int(input("enter the first value: "))
+j = int(input("enter the second value: "))
+z = h//j
+print('value after division is:',z)
+print(type(h))
+print(type(j))
+print(type(z))
+"""
+
+
+"""
+6: Find sum of two numbers x and y and display the message: “Sum of x and y
+is sum_value”. Obtain values for x and y from the user and use x, y variables
+inside the print() to display the message. (sum_value should be replaced with the
+sum value obtained.)
+"""
+"""
+x = int(input("enter the first value: "))
+y = int(input("enter the second value: "))
+#z = x + y
+print('Sum of',x,'and',y,'is',x+y)
+"""
+
+
+
+

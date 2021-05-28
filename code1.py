1722f8849b7cdec440257c3bb84037d764d8de00
joycethomas-123 <78149448+joycethomas-123@users.noreply.github.com>
2021-05-28 14:12:41 +0530

    Create code1.py

---
+++ code1.py
@@ -0,0 +1,8 @@
+#my first code in python
+# Display product of any two numbers by taking value from the user.
+
+"""
+x = input("enter the first number: ")
+y = input("enter the second number: ")
+print(int(x) * int(y))
+"""

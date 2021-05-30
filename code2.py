6a6b2f035d60694a19aef6e27bcb1c3aaab3450a
joycethomas-123 <78149448+joycethomas-123@users.noreply.github.com>
2021-05-30 17:19:21 +0530

    Create code2.py

---
+++ code2.py
@@ -0,0 +1,89 @@
+"""
+Q1. Write a program to accept percentage marks from the user and display the
+grade according to the following criteria:
+Marks Grade
+> 90 A
+> 80 and <= 90 B
+>= 60 and <= 80 C
+below 60 D
+"""
+"""
+x = int(input("enter the mark: "))
+if x>90:
+    print("grade = A")
+elif x > 80 and x <= 90:
+    print("grade = B")
+elif x>60 and x<=80:
+    print("grade = C")
+elif x<60:
+    print("grade = D")
+else:
+    print("failed")
+"""
+
+
+"""
+Q2. Write a program to check whether a year (given by user) is leap year or not.
+"""
+"""
+x=int(input("Enter a year: "))
+if x%4==0 and x%100!=0 or x%400==0:
+    print(x,"a leap year")
+else:
+    print(x,"not a leap year")
+ """
+
+
+"""
+Q3. Write a program to accept a number from 1 to 7 and display the name of the
+day like 1 for Sunday, 2 for Monday and so on.
+"""
+"""
+x = int(input("enter a number with a range of 1 to 7: "))
+if x == 1:
+    print("sunday")
+elif x == 2:
+    print("monday")
+elif x==3:
+    print("tuesday")
+elif x==4:
+    print("wednesday")
+elif x==5:
+    print("thursday")
+elif x==6:
+    print("friday")
+elif x==7:
+    print("saturday")
+else:
+    print("no such day")
+"""
+
+"""
+Q4. Write a program to accept a positive number from the user and display if it is
+an even number or odd number.
+"""
+"""
+x = int(input("enter a positive number: "))
+
+if x%2==0:
+    print("The number is even")
+else:
+    print("The number is odd")
+ """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
